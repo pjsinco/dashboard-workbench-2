afe896e51ec537e94d42bc3e71735d71d779f1c1
pjsinco <psinco@gmail.com>
2017-09-26 12:15:16 -0500

    Start unit-testing with jest

---
+++ src/components/MessageToggle.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    {{ message }}
+  </span>
+</template>
+
+<script>
+  export default {
+
+    name: 'messageToggle',
+
+    data () {
+
+      return {
+        message: 'message'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
+
